3917a97b5a5df905180899c2e3210c3a30ca21b2
Andy Kaier <akaier@princeton.edu>
2012-05-30 13:13:38 -0400

    More site improvements, added new pages for link menu

--- danes/views.py
+++ danes/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 def home(request):
     context = {}
     return render_to_response("home.html", context)
+
+def winter(request):
+    context = {}
+    return render_to_response("winter.html", context)
+
+def summer(request):
+    context = {}
+    return render_to_response("summer.html", context)
+
+def showcases(request):
+    context = {}
+    return render_to_response("showcases.html", context)
